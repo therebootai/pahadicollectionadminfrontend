663137e6a7eb24661199ef0af9ca5de577c8d440
fireFistAniket <sahaaniket720@gmail.com>
2025-01-31 17:50:45 +0530

    component added

--- src/pages/components/WebComponents.jsx
+++ src/pages/components/WebComponents.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import MainPageTemplate from "../../template/MainPageTemplate";
 import PaginationBox from "../../components/global/PaginationBox";
+import AddNewComponent from "../../components/webcomponents/AddNewComponent";
+import { useParams } from "react-router-dom";
 
 const WebComponents = () => {
+  const { type } = useParams();
+
   return (
     <MainPageTemplate>
       <div className="flex flex-row gap-6 items-center border-b border-custom-gray-border xl:px-8 px-6 p-4">
@@ -11,9 +15,15 @@ const WebComponents = () => {
         </button>
       </div>
       <div className="m-6 p-6 flex flex-col gap-6 bg-white rounded border border-custom-gray-border">
-        <h1 className="text-2xl font-medium text-custom-black">Slider</h1>
+        <h1 className="text-2xl font-medium text-custom-black capitalize">
+          {type}
+        </h1>
+        <AddNewComponent pageType={type} />
+        
       </div>
-      <PaginationBox />
+      <div className="m-6">
+        <PaginationBox />
+      </div>
     </MainPageTemplate>
   );
 };
